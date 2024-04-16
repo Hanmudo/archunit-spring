82ee9fe61944d2741ec6a05feecbc0c8ea29e45c
rrijn <aC9595496!>
2024-04-16 21:09:58 +0200

    feat: Upload clean starter spring project

---
+++ src/main/java/com/example/archunitspring/ArchunitSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.archunitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchunitSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchunitSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/archunitspring/ArchunitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.archunitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchunitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
